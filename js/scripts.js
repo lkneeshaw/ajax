6f36c341290b0cc6101f53ea0f9854459c5d327e
lkneeshaw <lkneeshaw@vcd-ceb131-12.eastern.ewu.edu>
2017-05-04 16:41:31 -0700

    Added Airtable

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
+// @codekit-prepend "airtable.js";
 
 // `load()` method
 //$('#data').load('data/data.txt');
 
 //$('#data').load('data/data.html');
 
-$('#data').load('data/data.json');
+//$('#data').load('data/data.json');
+
+// Configure Airtable - Handshake
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyCYIL8w8qxlK7UU'}).base('appnoFkpdhB8Bf1aE');
+
+// AJAX Call
+
+base('Entertainers').select({
+    view: 'Grid view'
+}).firstPage(function(err, records) {
+    if (err) { console.error(err); return; }
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('Name'));
+    });
+});
